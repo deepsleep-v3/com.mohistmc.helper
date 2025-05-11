1a0b75ad5feeaf17fe60cb35d5aafd78f0afa7a8
deepsleep-v3 <wontfix@hotmail.com>
2025-05-11 14:31:40 +0800

    Update Main.java

--- src/main/java/com/mohistmc/helper/Main.java
+++ src/main/java/com/mohistmc/helper/Main.java
@@ -2,15 +2,9 @@ package com.mohistmc.helper;
 
 public class Main {
 	public static void main(String[] args){
-		if (args[0] == "/mohistmc"){
-			switch(args[1]){
-				case "1.7.10":
-					System.out.println("此版本已被弃用。");
-					break;
-				case "1.12.2":
-					throw new Exception("Not implements yet.")
-					break;
-			}
+		boolean ansi = false;
+		if (args[0].equalsIgnoreCase("-ANSI")){
+			ansi = true;
 		}
 	}
 }
