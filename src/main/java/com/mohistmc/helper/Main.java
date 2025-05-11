c01a73b1f30143d73b2c4d8bf297794fff43fbef
deepsleep-v3 <wontfix@hotmail.com>
2025-05-11 14:44:04 +0800

    Update Main.java

--- src/main/java/com/mohistmc/helper/Main.java
+++ src/main/java/com/mohistmc/helper/Main.java
@@ -3,6 +3,7 @@ package com.mohistmc.helper;
 public class Main {
 	public static void main(String[] args){
 		boolean ansi = false;
+		
 		if (args[0].equalsIgnoreCase("-ANSI")){
 			ansi = true;
 		}
