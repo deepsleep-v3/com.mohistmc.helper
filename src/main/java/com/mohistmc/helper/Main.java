f75a91956dbdf6177333fcf7df3cabb0268fb8f5
deepsleep-v3 <wontfix@hotmail.com>
2025-05-12 20:17:31 +0800

    Update Main.java. ANSI Warning 1st.

--- src/main/java/com/mohistmc/helper/Main.java
+++ src/main/java/com/mohistmc/helper/Main.java
@@ -3,8 +3,8 @@ package com.mohistmc.helper;
 public class Main {
 	public static void main(String[] args){
 		boolean ansi = false;
-		
 		if (args[0].equalsIgnoreCase("-ANSI")){
+			System.out.println("WARNING: If your console isn't support ANSI Escape Features");
 			ansi = true;
 		}
 	}
