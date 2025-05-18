e50eea8a4c54dd77b40802d4fe1f0ce5f986ac30
deepsleep-v3 <wontfix@hotmail.com>
2025-05-18 10:53:14 +0800

    Create ANSITest.java

---
+++ src/main/java/com/mohistmc/helper/ANSITest.java
@@ -0,0 +1,7 @@
+package com.mohistmc.helper;
+
+public class ANSITest{
+	public static boolean App(){
+		System.out.println("This is a test text. If you see this text in color, it means your console supports ANSI escape character functionality.");
+	}
+}
